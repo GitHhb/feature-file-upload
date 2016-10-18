41d02c5f3cd42fa75cd21dd1bc86a089e19176ef
GitHhb <github@mittere.net>
2016-10-19 00:41:12 +0200

    Initial commit

---
+++ src/main/java/eu/centric/UploadController.java
@@ -0,0 +1,32 @@
+package eu.centric;
+
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Controller
+public class UploadController extends WebMvcConfigurerAdapter {
+	
+	@GetMapping("/")
+	public  String mainPage(Model model) {
+		System.out.println("here");
+		return "mypage.html";
+	}
+		 
+//	@Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+//       // registry.addViewController("/mypage").setViewName("mypage");
+//        registry.addViewController("/").setViewName("mypage");
+//	}
+	
+	/*
+	    public void addViewControllers(ViewControllerRegistry registry) {
+	        registry.addViewController("/").setViewName("mypage");
+	        registry.addViewController("/mypage").setViewName("mypage");
+	    }
+*/
+}
---
+++ src/test/java/eu/centric/FeatureFileUploadApplicationTests.java
@@ -0,0 +1,16 @@
+package eu.centric;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FeatureFileUploadApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
